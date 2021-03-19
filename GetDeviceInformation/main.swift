c1c7216d6156dba75bdec61152895243c205b507
Shakshi3104 <pikanchisato@yahoo.co.jp>
2021-03-19 18:36:35 +0900

    Init commit

--- GetDeviceInformation/main.swift
+++ GetDeviceInformation/main.swift
@@ -6,6 +6,15 @@
 //
 
 import Foundation
+import DeviceHardware
 
-print("Hello, World!")
+let device = MacDeviceHardware.deviceHardware
 
+print("------ Mac Device Hardware Information ------")
+print("Model Name      : \(device.modelName)")
+print("Processor       : \(device.processorName)")
+print("CPU             : \(device.cpu)")
+print("Processor Count : \(device.processorCount)")
+print("GPU             : \(device.gpu)")
+print("Neural Engine   : \(device.neuralEngine)")
+print("RAM             : \(device.ramString)")
